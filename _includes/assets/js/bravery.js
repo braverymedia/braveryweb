0e2084993869e899f20dc4b3069be6e1b8d08d6a
Joel Goodman <joel@braverymedia.co>
2020-10-31 16:49:01 -0500

    implement sass processing and add CSS-only fake browser frame

--- _includes/assets/js/bravery.js
+++ _includes/assets/js/bravery.js
@@ -37,7 +37,7 @@ if (closeContact) {
 // Lazy loading images
 const images = document.querySelectorAll('[data-src]');
 const config = {
-    rootMargin: '0px 0px 50px 0px',
+    rootMargin: '50px',
     threshold: 0
 };
 let observer = new IntersectionObserver(function (entries, self) {
@@ -72,3 +72,36 @@ triggerMenu.addEventListener('click', e => {
         triggerMenu.setAttribute('aria-expanded', "true");
     }
 });
+
+// scroll faux browser image
+const fakeBrowser = document.querySelector('.faux-browser');
+// Intersection Observer Configuration
+const observerOptions = {
+    root: null,
+    rootMargin: '-50px 0px -50px 0', // important: needs units on all values
+    threshold: 0
+};
+// Intersection Observer Constructor.
+const fbObserver = new IntersectionObserver(
+    handleIntersect,
+    observerOptions
+);
+// Intersection Observer Callback Function
+function handleIntersect(entry) {
+    // If intersecting.
+    if (entry[0].intersectionRatio > 0) {
+        simulateScroll(fakeBrowser);
+        console.log('Element is Intersecting');
+    } else {
+        console.log('Element is NOT Intersecting');
+    }
+};
+function simulateScroll( target ) {
+    let img = target.querySelector('img');
+    window.addEventListener("scroll", function () {
+        img.style.transform = `translateY(${2 *
+            -1}px)`;
+    });
+}
+
+fbObserver.observe(fakeBrowser);
