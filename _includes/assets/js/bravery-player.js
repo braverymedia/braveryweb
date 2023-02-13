0c73b8ff0284ccfdf11e79556a4341dc9915a0d4
Joel Goodman <joel@bravery.co>
2023-02-13 13:17:04 -0500

    missing js reference

---
+++ _includes/assets/js/bravery-player.js
@@ -0,0 +1,62 @@
+const audioElement = document.querySelector("audio");
+const audioContext = new AudioContext();
+const track = audioContext.createMediaElementSource(audioElement);
+
+// Select our play button
+const playPause = document.querySelector("#play-pause");
+const progress = document.querySelector("progress");
+const playerCurrentTime = document.querySelector(".audio-current-time");
+const trackDuration = document.querySelector(".audio-duration");
+const totalTime = playPause.dataset.duration;
+
+window.addEventListener("DOMContentLoaded", () => {
+	track.connect(audioContext.destination);
+
+	const setTimes = () => {
+		playerCurrentTime.textContent = new Date(audioElement.currentTime * 1000)
+			.toISOString()
+			.substring(11, 8)
+		trackDuration.textContent = new Date(totalTime * 1000)
+			.toISOString()
+			.substring(11, 8)
+	}
+
+	setTimes();
+	playPause.addEventListener(
+		"click",
+		() => {
+			// Check if context is in suspended state (autoplay policy)
+			if (audioContext.state === "suspended") {
+				audioContext.resume();
+			}
+
+			// Play or pause track depending on state
+			if (playPause.dataset.playing === "false") {
+				audioElement.play();
+				playPause.dataset.playing = "true";
+			} else if (playPause.dataset.playing === "true") {
+				audioElement.pause();
+				playPause.dataset.playing = "false";
+			}
+		},
+		false
+	);
+	const progressUpdate = (e) => {
+		const percent = (audioElement.currentTime / trackDuration) * 100;
+		progress.value = percent;
+	};
+	// Update progress bar and time values as audio plays
+	audioElement.addEventListener("timeupdate", () => {
+		progressUpdate();
+		setTimes();
+	});
+    audioElement.addEventListener("ended", () => {
+        playButton.dataset.playing = "false";
+        pauseIcon.classList.add("hidden");
+        playIcon.classList.remove("hidden");
+        progress.value = 0;
+        audioElement.currentTime = 0;
+        audioElement.duration = trackDuration;
+    });
+	audioElement.addEventListener("timeupdate", progressUpdate);
+});
